e10e7083432c42fb3e3dde64f17cbf574a18eb77
Carlos Maturana <caloja.sun@gmail.com>
2017-12-11 09:22:46 -0500

    Ajuste de Componentes visuales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TheatersComponent } from './theaters/theaters.component';
+import { ContentComponent } from './content/content.component';
+
+// Application Imports
+import {MoviesService} from './services/movies.service';
+import {TheatersService} from './services/theaters.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { TheatersMoviesComponent } from './theaters-movies/theaters-movies.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoviesComponent,
+    TheatersComponent,
+    ContentComponent,
+    TheatersMoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ MoviesService, TheatersService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
